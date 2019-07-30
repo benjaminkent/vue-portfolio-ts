4fd152683f851dfa13c28bdbef81d93f27b2cb63
benj <benj@knowbe4.com>
2019-07-30 15:53:33 -0400

    base setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<template lang="pug">
+  .home-container
+    .big-header
+      header
+        ul
+          li Home
+          li About
+          li Portfolio
+          li Resume
+          li Blog
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from 'vue-property-decorator'
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
+@Component({})
 export default class Home extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+.big-header {
+  header {
+    background-color: #333;
+    padding: 10px 0;
+    ul {
+      margin: 0 20px 0 0;
+      padding: 0;
+      list-style-type: none;
+      display: flex;
+      justify-content: flex-end;
+      li {
+        color: #fff;
+        margin: 0 10px;
+      }
+    }
+  }
+}
+</style>
+
