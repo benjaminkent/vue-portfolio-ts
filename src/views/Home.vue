8cdd2e7952deb24fd917b4b30022f5456120a79b
benj <benj@knowbe4.com>
2019-07-30 16:43:05 -0400

    making good headway on header and mobile optimization

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,23 @@
   .home-container
     .big-header
       header
+        .logo
+          i.fad.fa-narwhal
         ul
           li Home
           li About
           li Portfolio
           li Resume
           li Blog
+    .mobile-header
+      header
+        .logo
+          i.fad.fa-narwhal
+        .hamburger
+          .hamburger-line
+          .hamburger-line
+          .hamburger-line
+    .jumbotron
 </template>
 
 <script lang="ts">
@@ -20,19 +31,73 @@ export default class Home extends Vue {}
 <style lang="scss" scoped>
 .big-header {
   header {
-    background-color: #333;
+    // background-color: #333;
+    // color: #fff;
+    position: absolute;
+    width: 100%;
     padding: 10px 0;
+    display: flex;
+    justify-content: space-between;
+    .logo {
+      display: flex;
+      margin-left: 20px;
+      i {
+        font-size: 17px;
+        margin-right: 10px;
+      }
+    }
     ul {
-      margin: 0 20px 0 0;
+      margin: 0;
       padding: 0;
       list-style-type: none;
       display: flex;
       justify-content: flex-end;
       li {
-        color: #fff;
-        margin: 0 10px;
+        margin: 0 20px;
       }
     }
+  }
+}
+
+.mobile-header {
+  position: absolute;
+  padding: 10px 0;
+  width: 100%;
+  background-color: #333;
+  color: #fff;
+  i {
+    margin-left: 20px;
+  }
+}
+.jumbotron {
+  background-image: url('../assets/desk-one.jpeg');
+}
+
+@media (min-width: 551px) {
+  .mobile-header {
+    display: none;
+  }
+
+  .jumbotron {
+    height: 1000px;
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    display: grid;
+    grid-template-rows: repeat(5, 1fr);
+    grid-template-columns: repeat(5, 1fr);
+  }
+}
+
+@media (max-width: 550px) {
+  .big-header {
+    display: none;
+  }
+
+  .jumbotron {
+    height: 500px;
+    background-size: cover;
+    background-position: center;
   }
 }
 </style>
