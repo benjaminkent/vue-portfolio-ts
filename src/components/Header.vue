621b5c45e439268952357fa7e1a452e0478a62e5
benjaminkent <benjamin.k.jehl@gmail.com>
2019-08-01 10:09:02 -0400

    change mobile links to match

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,9 +16,9 @@
           ul
             li(@click='goHome') Home
             li(@click='mobileGoAbout') About
+            li Toolset
             li Portfolio
             li Resume
-            li Toolset
       header
         .logo
           i.fad.fa-narwhal
