ed2f1e9e59f71fa981db4bfb1a04137ab08e7d78
benjaminkent <benjamin.k.jehl@gmail.com>
2019-08-01 17:26:54 -0400

    hard-coded card done

--- src/components/toolset/Card.vue
+++ src/components/toolset/Card.vue
@@ -5,9 +5,11 @@
         img.logo(src='../../assets/vue-logo.png')
         h3 Vue.js
         p Vue is a progressive framework for building user interfaces. It is designed from the ground up to be incrementally adoptable.
-        .link
-          a(href='https://vuejs.org/v2/guide/') LEARN MORE
-          i.far.fa-arrow-right
+        .link(@mouseover='showArrow = true' @mouseleave='showArrow = false')
+          a(href='https://vuejs.org/v2/guide/' target='_blank' rel='noreferrer noopener')
+            | LEARN MORE
+            transition(name='slide-right')
+              i.far.fa-arrow-right(v-if='showArrow')
 </template>
 
 <script lang="ts">
@@ -26,7 +28,9 @@ export default class Card extends Vue {
 .card {
   width: 90%;
   background-color: #fff;
+  border: 1px solid #ddd;
   border-top: 2px solid $blue;
+  transition: 0.3s all ease-in-out;
   .card-content {
     margin: 30px;
     .logo {
@@ -43,6 +47,7 @@ export default class Card extends Vue {
       font-size: 14px;
       color: $blue;
       margin-top: 25px;
+      width: 120px;
       a {
         color: $blue;
         text-decoration: none;
@@ -53,6 +58,21 @@ export default class Card extends Vue {
       }
     }
   }
+}
+.slide-right-enter-active {
+  transition: all 0.3s ease-out;
+}
+.slide-right-leave-active {
+  transition: all 0.3s ease-in-out;
+}
+.slide-right-enter,
+.slide-right-leave-to {
+  transform: translateX(-20px);
+  opacity: 0;
+}
+.card:hover {
+  box-shadow: 2px 3px 6px 5px #22222240;
+  transition: 0.6s all ease-in-out;
 }
 @media (max-width: 550px) {
   .card-container {
