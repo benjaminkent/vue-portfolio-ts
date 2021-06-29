87431f8d6174b943fed8934ac8a34a2f2b797787
benjaminkent <benjamin.k.jehl@gmail.com>
2021-06-28 20:52:56 -0400

    update copy and change color scheme

--- src/components/about/Philosophy.vue
+++ src/components/about/Philosophy.vue
@@ -5,7 +5,7 @@
         .tech-message-header
           h3 MINDSET
           h2 Tech Philosophy
-        p This website was designed, coded and styled completely from scratch by myself using the Vue.js JavaScript framework. With most of my personal projects, I prefer to write all of the code from scratch, although I have used CSS frameworks such as Bulma and Tailwind CSS to style websites as well; in my professional career I work extensivly with Bootstrap. I believe in solving problems the right way. Which means writing easily maintainable code that doesn't add to tech debt and actively seeks to eliminate it. I am extremely excited about where the tech industry is heading and look forward to the many challenges and breakthroughs that lie before us.
+        p This website was designed, coded and styled completely from scratch by myself using the Vue.js JavaScript framework. With many of my personal projects, I prefer to write all of the code from scratch including CSS. Although I have used CSS and component library frameworks such as Quasar, Vue Bootstrap (Bootstrap), Buefy (Bulma) and Tailwind CSS to style websites as well; in my professional career I have worked extensivly with Bootstrap and Syncfusion. I believe in solving problems the right way. Which means writing easily maintainable code that doesn't add to tech debt and actively seeks to eliminate it. I am extremely excited about where the tech industry is heading and look forward to the many challenges and breakthroughs that lie before us.
       img.code-img(src='../../assets/code-one.jpeg')
 </template>
 
