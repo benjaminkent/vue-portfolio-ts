48ed263c54b9100d7a0732c8ca66e94e0ae27211
benjaminkent <benjamin.k.jehl@gmail.com>
2021-08-06 18:35:12 -0400

    pre-render spa and vue meta dont seem to be working with google crawler, moving meta tags to index html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -33,46 +33,5 @@ export default Vue.extend({
     AppFooter,
     DarkModeFab,
   },
-  metaInfo: {
-    title: 'Ben Jehl',
-    titleTemplate: '%s | Software Engineer',
-    meta: [
-      // tslint doesn't like the line length or " " here
-      // but it's fine. turning it off for the rest of this file
-      /* tslint:disable */
-      {
-        name: 'description',
-        content:
-          "Ben Jehl's portfolio website. Created from scratch with Vue.js and Typescript. Check out the repository on Github at https://github.com/benjaminkent/vue-portfolio-ts.",
-      },
-      { property: 'og:title', content: 'Ben Jehl | Software Engineer' },
-      { property: 'og:site_name', content: 'Ben Jehl' },
-      { property: 'og:type', content: 'website' },
-      { property: 'og:url', content: 'https://benkent.io' },
-      {
-        property: 'og:description',
-        content:
-          "Ben Jehl's portfolio website. Created from scratch with Vue.js and Typescript. Check out the repository on Github at https://github.com/benjaminkent/vue-portfolio-ts.",
-      },
-      { name: 'twitter:card', content: "Ben Jehl's portfolio website." },
-      { name: 'twitter:site', content: 'https://benkent.io' },
-      { name: 'twitter:title', content: 'Ben Jehl | Software Engineer' },
-      {
-        name: 'twitter:description',
-        content:
-          "Ben Jehl's portfolio website. Created from scratch with Vue.js and Typescript. Check out the repository on Github at https://github.com/benjaminkent/vue-portfolio-ts.",
-      },
-      { name: 'twitter:creator', content: '@BenjaminJehl' },
-      { itemprop: 'name', content: 'Ben Jehl | Software Engineer' },
-      {
-        itemprop: 'description',
-        content:
-          "Ben Jehl's portfolio website. Created from scratch with Vue.js and Typescript. Check out the repository on Github at https://github.com/benjaminkent/vue-portfolio-ts.",
-      },
-    ],
-    link: [{ rel: 'canonical', href: 'https://benkent.io' }],
-  },
 })
 </script>
-
-<style lang="scss" scoped></style>
