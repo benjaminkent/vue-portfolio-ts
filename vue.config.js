b9345c167db0ed2c15db9d770e77d9f4bab7eb2f
benjaminkent <benjamin.k.jehl@gmail.com>
2022-04-27 19:52:04 -0400

    remove migration build

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,6 @@
 const path = require('path')
 
 module.exports = {
-  chainWebpack: config => {
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap(options => {
-        return {
-          ...options,
-          compilerOptions: {
-            compatConfig: {
-              MODE: 2
-            }
-          }
-        }
-      })
-  },
-
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'scss',
